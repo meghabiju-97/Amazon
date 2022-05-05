663f88f71937a4e1740d793f0e1b8e123e5466d4
meghabiju-97 <megha.biju1997@gmail.com>
2022-05-05 07:42:16 +0530

    src

--- src/test/java/amazontest/AmazonTestcases.java
+++ src/test/java/amazontest/AmazonTestcases.java
@@ -2,11 +2,16 @@ package amazontest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.beust.jcommander.JCommander.Builder;
+
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import basefunctions.BaseClass;
@@ -25,15 +30,42 @@ public void tc01() throws InterruptedException {
 	driver.manage().window().maximize();
 	WebElement searchbox=driver.findElement(By.id("twotabsearchtextbox"));
 	System.out.println("getAttribute() method:" + searchbox.getAttribute("name"));
-	searchbox.sendKeys("samsung");
+	searchbox.sendKeys("iphone");
 	driver.findElement(By.xpath("//input[@id=\"nav-search-submit-button\"]")).submit();
+	
+	driver.navigate().to("http://demo.guru99.com/test/delete_customer.php");
+	driver.findElement(By.name("cusid")).sendKeys("53920");
+	driver.findElement(By.name("submit")).click();
+	driver.switchTo().alert().accept();
+	//Thread.sleep(4000);
+	driver.navigate().refresh();
+	//driver.switchTo().alert().dismiss();
+	
+	
+	
+	 /*driver .navigate().to("http://automationpractice.com/index.php?id_category=3&controller=category");
+	 Thread.sleep(2000);
+	 WebElement testDropDown=driver.findElement(By.id("selectProductSort"));
+	 Select dropdown=new Select(testDropDown);
+	 dropdown.selectByValue("name:desc");
+	 dropdown.selectByVisibleText("In stock");
+	 JavascriptExecutor jse=(JavascriptExecutor)driver;//driver is eligible to execute java script
+	 jse.executeScript("window.scrollBy(0,250)");
+	 WebElement productname=driver.findElement(By.xpath("//img[@class='replace-2x img-responsive' and @title='Faded Short Sleeve T-shirts']"));
+	 Actions action=new Actions(driver);
+	 action.moveToElement(productname).perform();
+	// action.dragAndDrop(testDropDown, productname)
+	 
+	 
+	 ////img[@class='replace-2x img-responsive' and @title='Faded Short Sleeve T-shirts']
+	 
 	/*List<WebElement> productnames=driver.findElements(By.xpath("//div[@class='sg-col-inner']//span[@class='a-size-medium a-color-base a-text-normal']"));
 	System.out.println("getText() method:");
 	for(int i=0;i<=productnames.size();i++) {
 	System.out.println(productnames.get(i).getText());	
 	}
-	*/
-	driver.navigate().to("https://www.redbus.in/");
+	
+	/*driver.navigate().to("https://www.redbus.in/");
 	driver.findElement(By.xpath("//input[@data-message='Please enter a source city']")).sendKeys("Kottayam");
 	Thread.sleep(1000);
 	driver.findElement(By.xpath("//ul[@class='autoFill homeSearch']//li")).click();
