db94de613dced4437c42e9e0c6210d3c35bc8323
meghabiju-97 <megha.biju1997@gmail.com>
2023-12-02 09:19:00 +0530

    first commit

---
+++ src/test/java/com/LoginTestcase.java
@@ -0,0 +1,38 @@
+package com;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTestcase {
+	
+	@Test
+	public void tc001() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		driver.get("https://www.amazon.in/");
+		
+		driver.findElement(By.cssSelector("input[placeholder='Search Amazon.in']")).sendKeys("iphone");
+		
+		driver.findElement(By.xpath("//input[@value='Go']")).click();
+		
+		List<WebElement> ele=driver.findElements(By.xpath("//div[contains(@data-asin,'B0') and @data-index>=3]"));
+		
+		
+		for(WebElement name:ele) {
+			System.out.println(name.getText());
+		}
+	}
+	
+
+}
